a7f8fae9c7073a4adce4702e492d7a79b4544376
JimRooney <github@jimrooney.com>
2023-09-12 17:15:41 +1200

    asdf

---
+++ navbar.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  document.body.innerHTML =
+    `<div class="topnav">
+          <a class="active" href="index.html">Home</a>
+          <a href="https://sites.google.com/a/jimrooney.com/www/flying-stuff/logbook?pli=1">Logbook</a>
+          <a href="https://balance.jimrooney.com">Balance</a>
+          </div>` + document.body.innerHTML
+}
