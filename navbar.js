fff4fd33daf20e496e302529d7b693ba6b2df9c7
JimRooney <github@jimrooney.com>
2023-12-01 18:11:31 +1300

    More links

--- navbar.js
+++ navbar.js
@@ -10,6 +10,8 @@ const textContent = `<div>
 <span class="BlueBox"><a class="active" href="index.html">Home</a></span>
 <span class="BlueBox"><a href="https://script.google.com/a/macros/jimrooney.com/s/AKfycbxoGRpmF6A06s8croIww3vvOPZnQbK0UG8IeQXilxSJm7TL2ZZR/exec">Logbook</a></span>
 
+<span class="BlueBox"><a href="https://glenorchyair.sharepoint.com/:x:/r/sites/datastore/_layouts/15/Doc.aspx?sourcedoc=%7BB4CD6D57-6916-47D6-BC13-0C26EF5FFB03%7D&file=Jim%20Rooney%20-%20F%26D%202023.xlsx&action=default&mobileredirect=true">F&D</a></span>
+<span class="BlueBox"><a href="https://docs.google.com/a/jimrooney.com/forms/d/e/1FAIpQLSdbIqbJrdQxsrLDfD8bvUyBlpYLEb8DOoMI7bghqg1zx3px2g/viewform">AddFlight</a></span>
 <span class="BlueBox"><a href="https://balance.jimrooney.com">Balance</a></span>
 <span class="BlueBox"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfy9G-UZK7TJqueo8wG8pRAlS2LrGOm29I9P50ogR7B33QVdA/viewform">Yoga</a>
 </div>`
