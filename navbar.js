6d7dbc75c3c283356090a7ff0aec0377fa78c72d
JimRooney <github@jimrooney.com>
2023-12-03 14:08:29 +1300

    DailyOps

--- navbar.js
+++ navbar.js
@@ -11,6 +11,7 @@ const textContent = `<div>
 <span class="BlueBox"><a href="https://script.google.com/a/macros/jimrooney.com/s/AKfycbxoGRpmF6A06s8croIww3vvOPZnQbK0UG8IeQXilxSJm7TL2ZZR/exec">Logbook</a></span>
 
 <span class="BlueBox"><a href="https://glenorchyair.sharepoint.com/:x:/r/sites/datastore/_layouts/15/Doc.aspx?sourcedoc=%7BB4CD6D57-6916-47D6-BC13-0C26EF5FFB03%7D&file=Jim%20Rooney%20-%20F%26D%202023.xlsx&action=default&mobileredirect=true">F&D</a></span>
+<span class="BlueBox"><a href="https://glenorchyair.sharepoint.com/SitePages/Daily-Ops.aspx">DailyOps</a></span>
 <span class="BlueBox"><a href="https://docs.google.com/a/jimrooney.com/forms/d/e/1FAIpQLSdbIqbJrdQxsrLDfD8bvUyBlpYLEb8DOoMI7bghqg1zx3px2g/viewform">AddFlight</a></span>
 <span class="BlueBox"><a href="https://max.jimrooney.com">MaxPerformance</a></span>
 <span class="BlueBox"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfy9G-UZK7TJqueo8wG8pRAlS2LrGOm29I9P50ogR7B33QVdA/viewform">Yoga</a>
