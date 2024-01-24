016e41f1d755045d234d379738ed26b450a6cfff
JimRooney <github@jimrooney.com>
2024-01-24 16:34:51 +1300

    Messing with AJAX

--- sheets.js
+++ sheets.js
@@ -1,23 +1,20 @@
 class DataLoader {
     constructor(options) {
-      const sheetID = "1ZQDN58WT5hdFVYiJA7yIJocm4vbc0Gw8ecJUj1pkYwg"
-      const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`
-      const sheetName = "Links"
-      const query = encodeURIComponent("Select *")
-      this.url = `${base}&sheet=${sheetName}&tq=${query}`
+      this.sheetID = options?.sheetID || "1ZQDN58WT5hdFVYiJA7yIJocm4vbc0Gw8ecJUj1pkYwg"
+      this.base = `https://docs.google.com/spreadsheets/d/${this.sheetID}/gviz/tq?`
+      this.sheetName = options?.sheetName || "Links"
+      this.query = encodeURIComponent("Select *")
       this.data = []
       document.addEventListener("DOMContentLoaded", () => this.load())
     }
   
     load() {
-      console.log("Fetching...")
-      console.log("URL: ", this.url)
-      fetch(this.url)
+      console.log("Fetch")
+      const url = `${this.base}&sheet=${this.sheetName}&tq=${this.query}`
+
+      fetch(url)
         .then((res) => res.text())
         .then((rep) => {
-/* The line `//   console.log("reply: ", rep)` is a commented out line of code. It is not doing
-anything in the code. It is likely used for debugging purposes, where the developer can uncomment
-the line to print the `rep` variable to the console for inspection. */
         //   console.log("reply: ", rep)
           const jsData = JSON.parse(rep.substring(47).slice(0, -2)) // remove extra (non-JSON) formatting
           console.log("data loaded: ", jsData)
